f16000bb5eb2aceda8ff76046f44e11549b688b3
virtualdj <virtualdj@users.noreply.github.com>
2025-11-03 18:58:17 +0100

    Ignora i prezzi ogni 15 minuti nell'archivio
    Con il mese di novembre 2025 l'archivio ZIP contiene ora 2 file, uno con i prezzi ogni 15 minuti e uno con i prezzi ogni 60 minuti.
    Questa modifica consente allo script di elaborare solamente i file coi prezzi orari.

--- pun-fasce.py
+++ pun-fasce.py
@@ -63,100 +63,110 @@ try:
 except:
 	raise SystemExit('ERRORE: il file scaricato dal sito non è valido.')
 
-# Enumera i file nell'archivio ZIP
-xlsFound = False
+# Verifica se ci sono i prezzi ogni 15 minuti nell'archivio
+xlsFileName = ''
 for f in archive.filelist:
-	# Controlla che esista un file Excel dentro lo ZIP
-	if f.filename.lower().endswith('xlsx'):    
-		# Recupera il file non compresso in memoria
-		xlsFound = True
-		xlfile = archive.open(f.filename)
-		
-		# Apre il file Excel
-		try:
-			wb = load_workbook(xlfile)
-			#wb = load_workbook('Anno2022.xlsx')
-		except:
-			raise SystemExit('ERRORE: file Excel non valido nel file scaricato dal sito.')
+	if f.filename.lower().endswith('_60.xlsx'):
+		xlsFileName = f.filename
+		break
 
-		# Trova il foglio prezzi
-		try:
-			sheet = wb['Prezzi-Prices']
-		except:
-			raise SystemExit('ERRORE: file Excel \'Prezzi\' non trovato nel file scaricato dal sito.')
+# Se non ci sono, estrae il primo file Excel disponibile
+if (xlsFileName == ''):
+	for f in archive.filelist:
+		if f.filename.lower().endswith('.xlsx'):
+			xlsFileName = f.filename
+			break
 
-		# Carica le festività
-		it_holidays = holidays.IT()
-
-		# Inizializza le variabili di conteggio
-		festivo = False
-		prev_dat = ''
-		prev_month = 0
-		f1 = []
-		f2 = []
-		f3 = []
-		monoorario = []
-
-		# Header output
-		print('Mese','MO (€/kWh)','F1 (€/kWh)','F2 (€/kWh)','F3 (€/kWh)','F23 (€/kWh)', sep='\t')
-
-		# Esamina le righe non vuote a partire dalla seconda
-		for row in range(2, sheet.max_row + 1):
-			if (sheet.cell(row, 1).value is None):
-				break
+# Verifica se almeno un nome di file è stato trovato
+if (xlsFileName != ''):
+  
+	# Recupera il file non compresso in memoria
+	xlfile = archive.open(xlsFileName)
 	
-			# Estrae i valori delle celle dell'Excel
-			dat = str(sheet.cell(row, 1).value) #YYYYMMDD
-			ora = sheet.cell(row, 2).value - 1 # 1..24
-			prezzo = sheet.cell(row, 3).value / 1000
-	
-			# Verifica se il giorno è cambiato
-			if (dat != prev_dat):
-				# Converte la stringa giorno in data
-				dat2 = date(int(dat[0:4]), int(dat[4:6]), int(dat[6:8]))
-		
-				# Verifica la festività
-				festivo = dat2 in it_holidays
-		
-				# Aggiorna il valore di data
-				prev_dat = dat
-	
-			# Verifica se il mese è cambiato
-			if (dat2.month != prev_month):
-				# Nuovo mese	
-				# Stampa i totali precedenti
-				if (prev_month > 0):
-					print(f'{prev_month}/{anno}', fmt_mean(monoorario), fmt_mean(f1), fmt_mean(f2), fmt_mean(f3), calc_f23(f2, f3), sep='\t')
-			
-				# Memorizza il nuovo mese
-				prev_month = dat2.month
-		
-				# Azzera i conteggi
-				f1.clear()
-				f2.clear()
-				f3.clear()
-				monoorario.clear()
-	
-			# Estrae la fascia oraria
-			#print("Len", len(f1), len(f2), len(f3))
-			fascia = get_fascia(dat2, festivo, ora)
-			if fascia == 3:
-				f3.append(prezzo)
-			elif fascia == 2:
-				f2.append(prezzo)
-			elif fascia == 1:
-				f1.append(prezzo)
-			monoorario.append(prezzo)
+	# Apre il file Excel
+	try:
+		wb = load_workbook(xlfile)
+	except:
+		raise SystemExit('ERRORE: file Excel non valido nel file scaricato dal sito.')
 
-		# Verifica se l'ultimo mese è completo
-		next_day = dat2 + timedelta(days=1)
-		if (next_day.month != prev_month):
-			# Mese completo, mostra statistiche
-			print(f'{prev_month}/{anno}', fmt_mean(monoorario), fmt_mean(f1), fmt_mean(f2), fmt_mean(f3), calc_f23(f2, f3), sep='\t')
+	# Trova il foglio prezzi
+	try:
+		sheet = wb['Prezzi-Prices']
+	except:
+		raise SystemExit('ERRORE: file Excel \'Prezzi\' non trovato nel file scaricato dal sito.')
+
+	# Carica le festività
+	it_holidays = holidays.IT()
+
+	# Inizializza le variabili di conteggio
+	festivo = False
+	prev_dat = ''
+	prev_month = 0
+	f1 = []
+	f2 = []
+	f3 = []
+	monoorario = []
+
+	# Header output
+	print('Mese','MO (€/kWh)','F1 (€/kWh)','F2 (€/kWh)','F3 (€/kWh)','F23 (€/kWh)', sep='\t')
+
+	# Esamina le righe non vuote a partire dalla seconda
+	for row in range(2, sheet.max_row + 1):
+		if (sheet.cell(row, 1).value is None):
+			break
+
+		# Estrae i valori delle celle dell'Excel
+		dat = str(sheet.cell(row, 1).value) #YYYYMMDD
+		ora = sheet.cell(row, 2).value - 1 # 1..24
+		prezzo = sheet.cell(row, 3).value / 1000
+
+		# Verifica se il giorno è cambiato
+		if (dat != prev_dat):
+			# Converte la stringa giorno in data
+			dat2 = date(int(dat[0:4]), int(dat[4:6]), int(dat[6:8]))
+	
+			# Verifica la festività
+			festivo = dat2 in it_holidays
+	
+			# Aggiorna il valore di data
+			prev_dat = dat
+
+		# Verifica se il mese è cambiato
+		if (dat2.month != prev_month):
+			# Nuovo mese	
+			# Stampa i totali precedenti
+			if (prev_month > 0):
+				print(f'{prev_month}/{anno}', fmt_mean(monoorario), fmt_mean(f1), fmt_mean(f2), fmt_mean(f3), calc_f23(f2, f3), sep='\t')
+		
+			# Memorizza il nuovo mese
+			prev_month = dat2.month
+	
+			# Azzera i conteggi
+			f1.clear()
+			f2.clear()
+			f3.clear()
+			monoorario.clear()
+
+		# Estrae la fascia oraria
+		#print("Len", len(f1), len(f2), len(f3))
+		fascia = get_fascia(dat2, festivo, ora)
+		if fascia == 3:
+			f3.append(prezzo)
+		elif fascia == 2:
+			f2.append(prezzo)
+		elif fascia == 1:
+			f1.append(prezzo)
+		monoorario.append(prezzo)
+
+	# Verifica se l'ultimo mese è completo
+	next_day = dat2 + timedelta(days=1)
+	if (next_day.month != prev_month):
+		# Mese completo, mostra statistiche
+		print(f'{prev_month}/{anno}', fmt_mean(monoorario), fmt_mean(f1), fmt_mean(f2), fmt_mean(f3), calc_f23(f2, f3), sep='\t')
 
 # Chiude l'archivio
 archive.close()
 
-# Controlla se il file è stato trovato
-if (not xlsFound):
+# Mostra un errore se non è stato trovato alcun file
+if (xlsFileName == ''):
 	raise SystemExit('ERRORE: nessun file Excel è stato trovato nel file scaricato dal sito.')
